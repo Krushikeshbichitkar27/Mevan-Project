e1e518a271a4d23847036744ba504c76f0aed337
Anudip <Anudip@DESKTOP-B5SE6SS>
2022-10-19 14:14:13 +0530

    commit

---
+++ Generic/src/main/java/com/NonGeneric/NonGeneric.java
@@ -0,0 +1,30 @@
+package com.NonGeneric;
+
+public class NonGeneric 
+{
+	Object ob;
+
+	public NonGeneric() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NonGeneric(Object ob) {
+		super();
+		this.ob = ob;
+	}
+
+	public Object getOb() {
+		return ob;
+	}
+
+	public void setOb(Object ob) {
+		this.ob = ob;
+	}
+
+	void showType() {
+		System.out.println("Type:" + ob.getClass().getName());
+	}
+
+}
+
